b1c49d4afae598cd2b44dbf492b774d705871457
WestCraneLi <1565024126@qq.com>
2023-07-06 23:38:42 +0800

    修改标题

--- src/script/setReadme.js
+++ src/script/setReadme.js
@@ -16,8 +16,11 @@ const setReadme = async () => {
     const article = `
 **Today**
 <blockquote>${sentence}</blockquote>
-<img src="https://media.giphy.com/media/hvRJCLFzcasrR4ia7z/giphy.gif" width="25" alt="手势"><h3>Hi there！ I'm WestCraneLi ~</h3><img src="https://emojis.slackmojis.com/emojis/images/1588866973/8934/hellokittydance.gif?1588866973" alt="Hi" width="30" />
-
+<h3>
+<img src="https://media.giphy.com/media/hvRJCLFzcasrR4ia7z/giphy.gif" width="25" alt="手势">
+Hi there! I'm WestCraneLi ~
+<img src="https://emojis.slackmojis.com/emojis/images/1588866973/8934/hellokittydance.gif?1588866973" alt="Hi" width="30" />
+</h3>
 <a href="https://github.com/WestCraneLi">
 <div align="right" >
   <img align="right" src="https://count.getloli.com/get/@:WestCraneLi" alt="WestCraneLi" />
